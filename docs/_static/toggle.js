16fff01c447c5f8f9bf226d8f06c35cbce5de24c
Kolya <docs@edi-n.com>
2020-10-22 17:03:13 +0300

    work

--- docs/_static/toggle.js
+++ docs/_static/toggle.js
@@ -1,39 +1,6 @@
 // js для кнопки темной темы
-document.addEventListener('DOMContentLoaded', function() {
+const chk = document.getElementById('chk');
 
-    function toggleCssMode(isDay) {
-        var mode = (isDay ? "Day" : "Night");
-        localStorage.setItem("css-mode", mode);
-
-        var daysheet = $('link[href="_static/pygments.css"]')[0];
-        daysheet.disabled = !isDay;
-
-        var nightsheet = $('link[href="_static/dark1.css"]')[0];
-        if (!isDay && nightsheet === undefined) {
-            var element = document.createElement("link");
-            element.setAttribute("rel", "stylesheet");
-            element.setAttribute("type", "text/css");
-            element.setAttribute("href", "_static/dark1.css");
-            document.getElementsByTagName("head")[0].appendChild(element);
-            return;
-        }
-        if (nightsheet !== undefined) {
-            nightsheet.sheet.disabled = isDay;
-        }
-    }
-
-    var initial = localStorage.getItem("css-mode") != "Night";
-    var checkbox = document.querySelector('input[name=mode]');
-
-    toggleCssMode(initial);
-    checkbox.checked = initial;
-
-    checkbox.addEventListener('change', function() {
-        document.documentElement.classList.add('transition');
-        window.setTimeout(() => {
-            document.documentElement.classList.remove('transition');
-        }, 1000)
-        toggleCssMode(this.checked);
-    })
-
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
 });
